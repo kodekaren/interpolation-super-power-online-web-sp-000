1528fa379d40ca535533f0015e15be7621dce7f2
karen ercoli <karenercoli@gmail.com>
2018-11-05 15:45:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbpw (color)
   puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
 end
-color = (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
